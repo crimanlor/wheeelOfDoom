19c9c0c763ec30f88ef5d1fbbdf597a3b7644684
Lorena <criado.manzaneque@gmail.com>
2020-10-19 11:30:37 +0200

    javascript

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+document.querySelector("#btn").addEventListener("click", selectCoder);
+
+function selectCoder() {
+    let coderId = Math.floor(Math.random() * 24);
+    let nodeResult = document.querySelector("#result");
+    document.c
+}
