d1be135151a0ad9156226bd10321629439fb5d36
René <renejfc@gmail.com>
2020-10-20 10:28:10 +0200

    js progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
 'use strict'
 
-document.querySelector("#btn").addEventListener("click", selectCoder);
+let actualCoders = ['Alejandra', 'Alvaro', 'Carmen', 'Stefanie', 'Gabriel', 'Giacomo', 'Giselle', 'Jackson', 'Jorge', 'Jose', 'Isma', 'Laura R', 'Laura R', 'Loredana', 'Lorena', 'Lucas', 'MaxPower', 'Mercedes', 'Moises', 'Olga', 'Quim', 'R', 'Sergio', 'Vanessa'];
 
-function selectCoder() {
-    let coderId = Math.floor(Math.random() * 24);
-    let nodeResult = document.querySelector("#result");
-    document.c
+const nodeSection = document.createElement('section');
+mainContent.appendChild(nodeSection).setAttribute('id', 'namesSection');
+const namesSectionId = document.querySelector('#namesSection');
+
+const nodeUl = document.createElement('ul');
+namesSectionId.appendChild(nodeUl).setAttribute('id', 'namesList');
+const nodeColumns = document.querySelector('#namesList');
+
+    for(let i = 0; i < actualCoders.length; i++) {
+    const nodeLi = document.createElement('li');
+    nodeColumns.appendChild(nodeLi).setAttribute('id', `coder${[i]}`);
+    
+    const coderId = document.querySelector(`#coder${[i]}`);
+    const nodeText = document.createTextNode(`${actualCoders[i]}`);
+    coderId.appendChild(nodeText);
+    
+
+}
+
+const btnListener = document.querySelector('#btn');
+btnListener.addEventListener('click', killEm);
+
+function killEm () {
+    let selection = Math.floor(Math.random() * actualCoders.length);
+    let loser = actualCoders[selection];
+
+    const resultId = document.querySelector('#result');
+    resultId.textContent = loser;
+    console.log(selection);
 }
